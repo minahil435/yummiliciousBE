6422bdd5abc59548760dd0c57d061eb89e12d83f
Minahil shafique <minahal.shafique@gmail.com>
2021-07-16 22:51:27 -0400

    bug in get command

--- routes/recipe/controller/recipeController.js
+++ routes/recipe/controller/recipeController.js
@@ -22,8 +22,9 @@ const getAllRecipes = async (req, res) => {
 const saveRecipe = async (req, res) => {
     try {
         const { idMeal, strMealThumb, strMeal } = req.body;
+        console.log(req.body)
 
-        const newRecipe = new Friend({
+        const newRecipe = new Recipe({
             idMeal,
             strMealThumb,
             strMeal,
@@ -34,16 +35,47 @@ const saveRecipe = async (req, res) => {
         const { decodedJwt } = res.locals;
         console.log(res.locals);
         //now we have to find the user ID
-        const foundTargetUser = await User.findOne({ email: decodedJwt.email });
-        foundTargetUser.friends.push(savedNewRecipe._id);
+        const foundTargetUser = await User.findOne({ email: decodedJwt.email })
+        
+        foundTargetUser.recipes.push(savedNewRecipe._id);
 
         await foundTargetUser.save();
 
-        res.json(savedNewFriend);
+        res.json(savedNewRecipe);
     } catch (e) {
         res.status(500).json({ e: e, message: e.message });
     }
 };
+
+
+const alreadylikedRecipe = async (req, res) => {
+    try {
+        const {idMeal} = req.body;
+        console.log("hi",req.body.idMeal)
+        const { decodedJwt } = res.locals;
+        const foundTargetUser = await User.findOne({ email: decodedJwt.email }).populate({
+            path: "recipes",
+            model: Recipe,
+            select: "-__v",
+        })
+        .select("-email -password -firstName -lastName -__v -_id -username");
+
+        let found = false
+
+         foundTargetUser.recipes.forEach(element => {
+            console.log(element.idMeal)
+            if (element.idMeal === idMeal) {
+                found = true
+          }
+        });
+
+    res.json({ message: "success", payload: found });
+    } catch (e) {
+        res.status(500).json({ e: e, message: e.message });
+    }
+};
+
+
 
 const deleteRecipeById = async (req, res, next) => {
     try {
@@ -68,5 +100,6 @@ const deleteRecipeById = async (req, res, next) => {
 module.exports = {
     getAllRecipes,
     saveRecipe,
-    deleteRecipeById
+    deleteRecipeById,
+    alreadylikedRecipe
 }
--- routes/recipe/recipeRouter.js
+++ routes/recipe/recipeRouter.js
@@ -7,10 +7,12 @@ const {
   saveRecipe,
   getAllRecipes,
   deleteRecipeById,
+  alreadylikedRecipe
 } = require("./controller/recipeController");
 
 router.get("/get-all-recipes", jwtMiddleware, getAllRecipes);
 router.post("/save-recipe", jwtMiddleware, saveRecipe);
 router.delete("/delete-recipe-by-id/:id", jwtMiddleware, deleteRecipeById);
+router.post("/already-liked-Recipe",jwtMiddleware,alreadylikedRecipe )
 
 module.exports = router;
